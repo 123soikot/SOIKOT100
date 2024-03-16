1998295c5b5c0870e0ee7d4e52e62560c053da1d
123soikot <162465476+123soikot@users.noreply.github.com>
2024-03-16 18:35:29 +0630

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,8 +28,8 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       :𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍
+𝐍𝐚𝐦𝐞       :SOIKOR
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : Ex 卝 বয়ফ্রেন্ডヅ
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐆𝐚𝐳𝐢𝐩𝐮𝐫, 𝐃𝐡𝐚𝐤𝐚
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐃𝐡𝐚𝐤𝐚 𝐆𝐚𝐳𝐢𝐩𝐮𝐫 𝐊𝐚𝐩𝐚𝐬𝐢𝐚
@@ -37,10 +37,10 @@ var callback = () => api.sendMessage({body:`
 𝐀𝐠𝐞           : 𝟏𝟖+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mdsohagkhan34562@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801815397046
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/sohagkhan422
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/cexvideo`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐆𝐦𝐚𝐢𝐥       : bdsoikotho@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.01758307316
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/soikot009
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/mdsakhoyat.hosen.9`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/61552819703988/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
