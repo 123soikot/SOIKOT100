4f17d680d1ee4b1cb799d180968775d2d0410592
123soikot <162465476+123soikot@users.noreply.github.com>
2024-03-31 19:48:53 +0630

    Create badol.js

---
+++ scripts/commands/badol.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs");
+module.exports.config = {
+  name: "badol",
+  version: "1.0.2",
+  permission: 0,
+  credits: "BADOL-KHAN",
+  description: "beginner's guide",
+  prefix: true,
+  category: "guide",
+  usages: "[Shows Commands]",
+  cooldowns: 5,
+  envConfig: {
+    autoUnsend: true,
+    delayUnsend: 60
+  }
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("git")==0 || (event.body.indexOf("😭")==0 || (event.body.indexOf("😓")==0 || (event.body.indexOf("python")==0)))) {
+		var msg = {
+				body: "Raja-Babu",
+				attachment: fs.createReadStream(__dirname + `/cache/kanna.mp3`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
