d700cc3f5f9d481d8705f6b7eb46ce7ac0e034a7
123soikot <162465476+123soikot@users.noreply.github.com>
2024-04-01 15:51:27 +0630

    Update 2npxs.js

--- scripts/commands/2npxs.js
+++ scripts/commands/2npxs.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("😭")==0 || event.body.indexOf("😓")==0 || event.body.indexOf("🥺")==0 || event.body.indexOf("😰")==0) {
 		var msg = {
 				body: "-!😓",
-				attachment: fs.createReadStream(__dirname + `/noprifix/Kanna.mp3`)
+				attachment: fs.createReadStream(__dirname + `/cache/Kanna.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
