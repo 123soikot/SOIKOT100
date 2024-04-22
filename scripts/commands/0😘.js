ba4f0a60a269a6808778bcceb50777649d92b4fa
123soikot <162465476+123soikot@users.noreply.github.com>
2024-04-22 11:14:41 +0630

    Create 0😘.js

---
+++ scripts/commands/0😘.js
@@ -0,0 +1,70 @@
+const axios = require("axios");
+const fs = require("fs");
+const request = require("request");
+
+const link = [
+"https://i.imgur.com/PzfipVy.mp4",
+
+];
+
+module.exports.config = {
+  name: "😘",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Rahad",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "😘",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  const content = event.body ? event.body : '';
+    const body = content.toLowerCase();
+  if (body.startsWith("😘")) {
+    const rahad = [
+      "_Boss ke dao.!🫂😍",
+      " 😘"
+    
+    ];
+    const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+
+    const callback = () => api.sendMessage({
+      body: `${rahad2}`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    
+    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+    return requestStream;
+  }
+};
+
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  const { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["😘"] === "undefined" || data["love"]) data["i love you"] = false;
+  else data["😘"] = true;
+  await Threads.setData(threadID, { data });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["😘"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
