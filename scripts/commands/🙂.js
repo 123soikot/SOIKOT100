75bfe5bebccb570a0030afa6a5ff7fc725556bb7
123soikot <162465476+123soikot@users.noreply.github.com>
2024-04-24 21:21:04 +0630

    Create 🙂.js

---
+++ scripts/commands/🙂.js
@@ -0,0 +1,59 @@
+module.exports.config = {
+  name: "🙂",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Rahad",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "🙂",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  const content = event.body ? event.body : '';
+    const body = content.toLowerCase();
+    if (body.startsWith("🙂")) {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var link = ["https://i.imgur.com/lx7urcI.mp4"];
+    var callback = () => api.sendMessage({
+      body: `__নতুন কে পেলে সবাই নিখঁজ🥺😓n\nEx 卝 বয়ফ্রেন্ডヅ🌺`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    const timeStart = Date.now();
+    const PREFIX = config.PREFIX;
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+  }
+};
+
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["🙂"] == "undefined" || data["😞"] == true) data["🖤"] = false;
+  else data["😓"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["😥"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
