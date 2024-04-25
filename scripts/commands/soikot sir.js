d648f358f57c2dad50c6e532295ecd3e0f3a6f0b
123soikot <162465476+123soikot@users.noreply.github.com>
2024-04-25 19:55:59 +0630

    Create soikot sir.js

---
+++ scripts/commands/soikot sir.js
@@ -0,0 +1,42 @@
+module.exports.config = {
+	name: "soikot sir",
+    version: "1.0.1",
+    permission: 0,
+    credits: "Nayan",
+    prefix: true,
+    description: "ask any thing",
+    category: "admin",
+    usages: "",
+    cooldowns: 5,
+    dependencies: {
+        "openai": ""
+};
+
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    let { messageID, threadID, senderID, body } = event;
+    let tid = threadID,
+    mid = messageID;
+    const content = encodeURIComponent(args.join(" "));
+    if (!args[0]) return api.sendMessage("───※ ·❆· ※───\n\nঅনুগ্রহ করে একটি বার্তা টাইপ করুন...\n\n───※ ·❆· ※───\nEx 卝 বয়ফ্রেন্ডヅ🔰", tid, mid);
+    try {
+        const res = await axios.get(`https://api.kenliejugarap.com/gptgo/?text=${content}`);
+        const respond = res.data.response;
+        if (res.data.error) {
+            api.sendMessage(`Error: ${res.data.error}`, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        } else {
+            api.sendMessage(respond, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        }
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while fetching the data.", tid, mid);
+    }
+};
