ef270eedcd7f9acfd41bcae201b39be8415d5220
123soikot <162465476+123soikot@users.noreply.github.com>
2024-05-02 14:32:10 +0630

    Update and rename bangla.js to bn.js

--- scripts/commands/bn.js
+++ scripts/commands/bn.js
@@ -1,5 +1,6 @@
+
 module.exports.config = {
-	name: "bangla",
+	name: "bn",
     version: "1.0.1",
     permission: 0,
     credits: "Nayan",
@@ -10,7 +11,7 @@ module.exports.config = {
     cooldowns: 5,
     dependencies: {
         "openai": ""
-    }
+	}
 };
 
 module.exports.run = async ({ api, event, args }) => {
@@ -22,18 +23,18 @@ module.exports.run = async ({ api, event, args }) => {
 	if (event.type == "message_reply") {
 		translateThis = event.messageReply.body
 		if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
-		else lang = 'bn';
+		else lang = global.config.language;
 	}
 	else if (content.indexOf(" -> ") == -1) {
 		translateThis = content.slice(0, content.length)
-		lang = 'fil';
+		lang = global.config.language;
 	}
-	return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${translateThis}`), (err, response, body) => {
-		if (err) return api.sendMessage("translate", event.threadID, event.messageID);
+	return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=bn&dt=t&q=${translateThis}`), (err, response, body) => {
+		if (err) return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
 		var retrieve = JSON.parse(body);
 		var text = '';
 		retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
 		var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
-		api.sendMessage(`English: ${text}\n - Translated ${fromLang} on ${lang}`, event.threadID, event.messageID);
+		api.sendMessage(`🌸বাংলা-কনভাট🌸:\n───※ ·❆· ※───\n\n ${text}\n\n───※ ·❆· ※───\n🔰Ex 卝 বয়ফ্রেন্ডヅ🔰`, event.threadID, event.messageID);
 	});
-    }
+	}
