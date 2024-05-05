9264ae64e41a912273390bfb061b300f55096220
123soikot <162465476+123soikot@users.noreply.github.com>
2024-05-05 15:11:28 +0630

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -1,47 +1,96 @@
+/**
+ * @author Nazrul
+ * @warn Do not edit code or edit credits
+ */
+
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    permission: 0,
-    credits: "nayan",
-    prefix: true,
-    description: "",
-    category: "prefix",
-    usages: "",
-    cooldowns: 5,
-    dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
+  name: "info",
+  version: "1.2.6",
+  hasPermssion: 0,
+  credits: "Nazrul",
+  usePrefix: true,
+  description: "info bot owner",
+  commandCategory: "Dành cho người dùng",
+  hide:true,
+  usages: "",
+  cooldowns: 5,
+};
+
+
+module.exports.run = async function ({ api, event, args, Users, permssion, getText ,Threads}) {
+  const content = args.slice(1, args.length);
+  const { threadID, messageID, mentions } = event;
+  const { configPath } = global.client;
+  const { ADMINBOT } = global.config;
+   const { NDH } = global.config;
+  const { userName } = global.data;
+  const request = global.nodemodule["request"];
+  const fs = global.nodemodule["fs-extra"];
+  const { writeFileSync } = global.nodemodule["fs-extra"];
+  const mention = Object.keys(mentions);
+  delete require.cache[require.resolve(configPath)];
+  var config = require(configPath);
+  const listAdmin = ADMINBOT || config.ADMINBOT || [];
+  const listNDH = NDH || config.NDH ||  [];
+  {
+    const PREFIX = config.PREFIX;
+    const namebot = config.BOTNAME;
+    const { commands } = global.client;
+    const threadSetting = (await Threads.getData(String(event.threadID))).data || 
+    {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX 
+    : global.config.PREFIX;
+    const dateNow = Date.now();
+    const time = process.uptime(),
+	      	hours = Math.floor(time / (60 * 60)),
+		      minutes = Math.floor((time % (60 * 60)) / 60),
+		      seconds = Math.floor(time % 60);
+    const data = [
+      "Bạn không thể tìm được lệnh admin tại 'help' của MintBot",
+      "Đừng mong chờ gì từ MintBot.",
+      "Cái đoạn này á? Của SpermBot.",
+      "Nếu muốn không lỗi lệnh thì hãy xài những lệnh có trong help vì những lệnh lỗi đã bị ẩn rồi.",
+      "Đây là một con bot được các coder của MiraiProject nhúng tay vào.",
+      "Muốn biết sinh nhật của Mint thì hãy xài 'birthday'.",
+      "Cặc.",
+      "Cút.",
+      "Lồn.",
+      "Bạn chưa biết.",
+      "Bạn đã biết.",
+      "Bạn sẽ biết.",
+      "Không có gì là hoàn hảo, MintBot là ví dụ.",
+      "Mirai dropped.",
+      "MintBot là MiraiProject nhưng module là idea của SpermBot.",
+      "Bạn không biết cách sử dụng MintBot? Đừng dùng nữa.",
+      "Muốn chơi game? Qua bot khác mà chơi đây không rảnh",
+      "MintBot có thể hiểu phụ nữ nhưng không thể có được họ.",
+      "MintBot cân spam nhưng không có gì đáng để bạn spam."
+    ];
+    var link = [
+      "YOUR IMGUR LINK",
+      
+ "YOUR IMGUR LINK",
+    ];
+    
+    var i = 1;
+    var msg = [];
+    const moment = require("moment-timezone");
+    const date = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
+    for (const idAdmin of listAdmin) {
+      if (parseInt(idAdmin)) {
+        const name = await Users.getNameUser(idAdmin);
+        msg.push(`${i++}/ ${name} - ${idAdmin}`);
+      }
+    }
+    var msg1 = [];
+            for (const idNDH of listNDH) {
+                if (parseInt(idNDH)) {
+                  const name1 = (await Users.getData(idNDH)).name
+                    msg1.push(`${i++}/ ${name1} - ${idNDH}`);
+                }
+            }
+    var callback = () => 
+      api.sendMessage({ body: `☘️𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂 𝗔𝗹𝗮𝗶𝗸𝘂𝗺💫\n\n🖤ADMIN AND BOT INFORMATION🌻.\n\nBOT NAME: =====「${global.config.BOTNAME}」=====\n\n😇BOT OWNER OF CEO😇\n──────────────\n☘️Ex 卝 বয়ফ্রেন্ডヅㅤ모ㅤᏴꫝʙʏ💫 (Fu3k)\nFB I'd :- https://www.facebook.com/mdsakhoyat.hosen.9\n\n☘️Ex 卝 বয়ফ্রেন্ডヅ모ㅤᏴꫝʙʏ💫 (𝐁𝐫𝐚𝐧𝐝)\nFB I'D :- https://www.facebook.com/mdsakhoyat.hosen.9\n\nBOT PREFIX : ${global.config.PREFIX}\n\nBOT OWNER : 『☘Ex 卝 বয়ফ্রেন্ডヅㅤ모ㅤᏴꫝʙʏ💫 (𝑴𝒂𝒍𝒔)』\n\n➟ UPTIME\n\nTODAY IS TIME :\n\nBOT IS RUNNING ${hours}:${minutes}:${seconds}.\n\n➟  TOTAL USER : ${global.data.allUserID.length}\n\n➟ TOTAL THREADS : ${global.data.allThreadID.length}\n\nTHANKS FOR USING ${global.config.BOTNAME} 『🙅🖤』`, attachment: fs.createReadStream(__dirname + "/cache/kensu.jpg"), }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/kensu.jpg"));
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/kensu.jpg")).on("close", () => callback()); 
   }
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`
---------------------------------------------
-𝐍𝐚𝐦𝐞       : SOIKOT(তো্ঁমা্ঁর্ঁ জা্ঁন্ঁ ত্যা্ঁহ্ঁ)
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : Ex 卝 বয়ফ্রেন্ডヅ
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: , 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: , 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥e
-𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : bdsoikotho@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801758307316
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/SOIKOT
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/mdsakhoyat.hosen.9`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-   };
