51aa909f9fe0b0967fd66545fb3e9fc3bd6508a3
123soikot <162465476+123soikot@users.noreply.github.com>
2024-07-08 18:30:11 +0600

    Update fbcover.js

--- scripts/commands/fbcover.js
+++ scripts/commands/fbcover.js
@@ -1,55 +1,74 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs");
+const axios = require("axios");
+
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
 
 module.exports.config = {
   name: "fbcover",
-  version: "1.0.0",
-  permssion: 0,
-  credits: "Ex 卝 বয়ফ্রেন্ডヅ",
-  description: "",
-  category: "fbcover",
-  prefix: true,
-    cooldowns: 2,
+  version: "6.9",
+  hasPermssion: 0,
+  credits: "Dipto",
+  description: "Facebook cover",
+  usePrefix: true,
+  category: "Cover",
+  usages: "name - title - address - email - phone - color (default = white)",
+  cooldowns: 5,
 };
-
-  module.exports.run = async function({ api, event, args, Users, Threads, Currencies}) {
-    const uid = event.senderID;
-    const info = args.join(" ");
-    const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
-  const n = apis.data.api
-    var id = Object.keys(event.mentions)[0] || event.senderID;
+module.exports.run = async function ({ api, event, args, Users }) {
+  const dipto = args.join(" ");
+  let id;
+  if (event.type === "message_reply") {
+    id = event.messageReply.senderID;
+  } else {
+    id = Object.keys(event.mentions)[0] || event.senderID;
+  }
   var nam = await Users.getNameUser(id);
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
-    if (!info) {
-      return api.sendMessage("Please enter in the format:\fbcover name - subname - address - email - phone nbr - color (default = no )", event.threadID);
-    } else {
-      const msg = info.split("-");
-      const name = msg[0].trim();
-      const subname = msg[1].trim();
-      const address = msg[2].trim();
-      const email = msg[3].trim();
-      const phone = msg[4].trim();
-      const color = msg[5].trim();
+  if (!dipto) {
+    return api.sendMessage(
+      `❌| wrong \ntry ${global.config.PREFIX}fbcover v1/v2/v3 - name - title - address - email - phone - color (default = white)`,
+      event.threadID,
+      event.messageID,
+    );
+  } else {
+    const msg = dipto.split("-");
+    const v = msg[0].trim() || "v1";
+    const name = msg[1].trim() || " ";
+    const subname = msg[2].trim() || " ";
+    const address = msg[3].trim() || " ";
+    const email = msg[4].trim() || " ";
+    const phone = msg[5].trim() || " ";
+    const color = msg[6].trim() || "white";
+    api.sendMessage(
+      `Processing your cover,Wait koro baby < 😘`,
+      event.threadID,
+      (err, info) =>
+        setTimeout(() => {
+          api.unsendMessage(info.messageID);
+        }, 4000),
+    );
+    const img = `${await baseApiUrl()}/cover/${v}?name=${encodeURIComponent(name)}&subname=${encodeURIComponent(subname)}&number=${encodeURIComponent(phone)}&address=${encodeURIComponent(address)}&email=${encodeURIComponent(email)}&colour=${encodeURIComponent(color)}&uid=${id}`;
 
-      api.sendMessage(`Processing your cover, please wait...`, event.threadID, (err, info) => setTimeout(() => { api.unsendMessage(info.messageID) }, 5000));
-
-      const img = `${n}/fbcover/v1?name=${encodeURIComponent(name)}&uid=${id}&address=${encodeURIComponent(address)}&email=${encodeURIComponent(email)}&subname=${encodeURIComponent(subname)}&sdt=${encodeURIComponent(phone)}&color=${encodeURIComponent(color)}`;
-
-      try {
-        const response = await axios.get(img, { responseType: 'arraybuffer' });
-        const image = await jimp.read(response.data);
-        const outputPath = `./fbcover_${uid}.png`;
-        await image.writeAsync(outputPath);
-        
-        const attachment = fs.createReadStream(outputPath);
-        api.sendMessage({ 
-          body: `◆━━━━━━━━◆◆━━━━━━━━◆\🔴INPUT NAME: ${name}\🔵INPUT SUBNAME:${subname}\📊ADDRESS: ${address}\✉️EMAIL: ${email}\☎️PHON NO.: ${phone}\🎇COLOUR: ${color}\🆔ID: ${nam}\◆━━━━━━━━◆◆━━━━━━━━◆`,
-          attachment
-        }, event.threadID, () => fs.unlinkSync(outputPath));
-      } catch (error) {
-        console.error(error);
-        api.sendMessage("An error occurred while generating the FB cover.", event.threadID);
-      }
+    try {
+      const response = await axios.get(img, { responseType: "stream" });
+      const attachment = response.data;
+      api.sendMessage(
+        {
+          body: `✿━━━━━━━━━━━━━━━━━━━━━━━━━━━✿\n🔵𝗙𝗜𝗥𝗦𝗧 𝗡𝗔𝗠𝗘: ${name}\n⚫𝗦𝗘𝗖𝗢𝗡𝗗 𝗡𝗔𝗠𝗘:${subname}\n⚪𝗔𝗗𝗗𝗥𝗘𝗦𝗦: ${address}\n📫𝗠𝗔𝗜𝗟: ${email}\n☎️𝗣𝗛𝗢𝗡𝗘 𝗡𝗢.: ${phone}\n☢️𝗖𝗢𝗟𝗢𝗥: ${color}\n💁𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘: ${nam}\n✅𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : ${v}\n✿━━━━━━━━━━━━━━━━━━━━━━━━━━━✿`,
+          attachment,
+        },
+        event.threadID,
+        event.messageID,
+      );
+    } catch (error) {
+      console.error(error);
+      api.sendMessage(
+        "An error occurred while generating the FB cover.",
+        event.threadID,
+      );
     }
-  };
+  }
+};
